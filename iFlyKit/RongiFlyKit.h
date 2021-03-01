7aa57dcd563e90670359cb42ce438054b89925de
qixinbing <qixinbing@rongcloud.cn>
2021-03-01 13:56:21 +0800

    update iflykit.h

--- iFlyKit/RongiFlyKit.h
+++ iFlyKit/RongiFlyKit.h
@@ -14,7 +14,7 @@ FOUNDATION_EXPORT double RongiFlyKitVersionNumber;
 //! Project version string for RongiFlyKit.
 FOUNDATION_EXPORT const unsigned char RongiFlyKitVersionString[];
 
-#if __has_include(<<RongiFlyKit/RCiFlyKit.h>>)
+#if __has_include(<RongiFlyKit/RCiFlyKit.h>)
 // iFlyKit核心类
 #import <RongiFlyKit/RCiFlyKit.h>
 #else
