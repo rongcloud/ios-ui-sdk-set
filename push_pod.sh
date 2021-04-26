6ed2998ab3a6cc7fc9697770734f2ca61f010bd9
qixinbing <qixinbing@rongcloud.cn>
2021-04-26 16:22:09 +0800

    update push_pod.sh

--- push_pod.sh
+++ push_pod.sh
@@ -5,4 +5,6 @@ git reset --hard origin/main
 git clean -dfx
 git pull
 
+sed -i ""  -e 's/[0-9]\.[0-9]\{1,2\}\.[0-9]\{1,2\}/'"$Version"'/' RongCloudOpenSource.podspec
+
 pod trunk push --use-libraries --allow-warnings --verbose
