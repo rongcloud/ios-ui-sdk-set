17d61f5c41e9c6f47c6944cc938fa67822d271c8
jenkins <jenkins@rongcloud.cn>
2022-06-08 20:51:02 +0800

    Release RongCloud SourceCode Version 5.2.3

---
+++ LocationKit/RongLocationKit.h
@@ -0,0 +1,20 @@
+//
+//  RongLocationKit.h
+//  RongLocationKit
+//
+//  Created by zgh on 2022/2/17.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RongLocationKit.RongLocationKitAdaptiveHeader
+FOUNDATION_EXPORT double RongLocationKitVersionNumber;
+
+//! Project version string for RongLocationKit.
+FOUNDATION_EXPORT const unsigned char RongLocationKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RongLocationKit/PublicHeader.h>
+#import <RongLocationKit/RCLocationMessageCell.h>
+#import <RongLocationKit/RCLocationPickerViewController.h>
+#import <RongLocationKit/RCLocationViewController.h>
+
