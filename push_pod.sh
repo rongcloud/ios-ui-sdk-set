1ae8fce7edf57dbd036724606b3683e5a1e2df48
zhanggaihong <zhanggaihong@rongcloud.cn>
2024-05-24 16:33:16 +0800

    update push_pod.sh

--- push_pod.sh
+++ push_pod.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+Private_Pods_Demo_Git=$1
+
 git reset --hard origin/main
 git checkout -f main
 git pull
@@ -14,5 +16,25 @@ fi
 
 git tag -a $Version -m $Version
 git push origin $Version
+
+pod repo update
+
+echo "******** 推送私有 pods 仓库开始 ********"
+# 私有 pods 仓库
+pod repo push spec RongCloudOpenSource.podspec --verbose --allow-warnings
+echo "******** 推送私有 pods 仓库结束 ********"
+
+echo "******** 验证私有 pods 仓库开始 ********"
+# demo 测试私有 pods
+PodsDemoPath="ios_privatepodsdemo/build.sh"
+# 检查文件是否存在
+if [ ! -e "$PodsDemoPath" ]; then
+   git clone $Private_Pods_Demo_Git
+fi
+
+cd ios_privatepodsdemo
+sh build.sh sourceCode
+cd ..
+echo "******** 验证私有 pods 仓库结束 ********"
 
 pod trunk push --use-libraries --allow-warnings --verbose
