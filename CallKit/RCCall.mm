d94c28a0c0888a79230568dc9441b3747cb8a148
jenkins <jenkins@rongcloud.net>
2024-06-26 15:59:49 +0800

    Release RongCloud SourceCode Version 5.8.2.100

--- CallKit/RCCall.mm
+++ CallKit/RCCall.mm
@@ -42,9 +42,9 @@
     }
 #endif
 
-static NSString *const __RongCallKit__Version = @"5.8.2_opensource";
-static NSString *const __RongCallKit__Commit = @"a684d06c9";
-static NSString *const __RongCallKit__Time = @"202406051748";
+static NSString *const __RongCallKit__Version = @"5.8.2.100_opensource";
+static NSString *const __RongCallKit__Commit = @"cde3aa31d";
+static NSString *const __RongCallKit__Time = @"202406261511";
 
 @interface RCCall () <RCCallReceiveDelegate>
 
--- IMKit/RCIM.m
+++ IMKit/RCIM.m
@@ -53,7 +53,7 @@ NSString *const RCKitDispatchConversationStatusChangeNotification =
 @end
 
 static RCIM *__rongUIKit = nil;
-static NSString *const RCIMKitVersion = @"5.8.2_opensource";
+static NSString *const RCIMKitVersion = @"5.8.2.100_opensource";
 @implementation RCIM
 
 + (instancetype)sharedRCIM {
