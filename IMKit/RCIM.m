0d3d51051bf9a47885a04cfa82b0476f2a8c3b97
jenkins <jenkins@rongcloud.net>
2024-07-31 18:04:42 +0800

    Release RongCloud SourceCode Version 5.10.2.100

--- IMKit/RCIM.m
+++ IMKit/RCIM.m
@@ -53,7 +53,7 @@ NSString *const RCKitDispatchConversationStatusChangeNotification =
 @end
 
 static RCIM *__rongUIKit = nil;
-static NSString *const RCIMKitVersion = @"5.10.2_opensource";
+static NSString *const RCIMKitVersion = @"5.10.2.100_opensource";
 @implementation RCIM
 
 + (instancetype)sharedRCIM {
