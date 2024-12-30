243550e6267afa6173352d61026ee54e7e653e45
zhanggaihong <zhanggaihong@rongcloud.cn>
2024-12-30 18:07:30 +0800

    feat: update push_pods.sh

--- push_pod.sh
+++ push_pod.sh
@@ -3,6 +3,7 @@
 trap exit ERR
 
 Private_Pods_Demo_Git=$1
+Only_Private_Verify=$2
 
 git reset --hard origin/main
 git checkout -f main
@@ -32,4 +33,6 @@ sh build.sh $Version sourcecode
 cd ..
 echo "******** 私有 pods 仓库操作结束 ********"
 
-pod trunk push --use-libraries --allow-warnings --verbose
+if [ "$Only_Private_Verify" != "true" ]; then
+    pod trunk push --use-libraries --allow-warnings --verbose
+fi
